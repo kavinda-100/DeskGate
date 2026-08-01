980a88b7e75efd88ed415314cb7fe70a2564883d
kavinda-100 <kavindarathnayake100@gmail.com>
2026-08-01 20:41:17 +0530

    Add reusable UI components and authentication schemas
    Introduced foundational UI components, including avatar, badge, card, checkbox, input, label, sheet, skeleton, and toast for consistent design usage. Auth schemas for sign-up and sign-in validations were also created and exported in the schemas package.

---
+++ packages/schemas/src/auth.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const signUpSchema = z
+  .object({
+    email: z.email({ error: 'Invalid email address' }),
+    password: z
+      .string()
+      .min(6, { error: 'Password must be at least 6 characters long' })
+      .max(12, { error: 'Password must be at most 12 characters long' }),
+    confirmPassword: z
+      .string()
+      .min(6, { error: 'Confirm Password must be at least 6 characters long' })
+      .max(12, { error: 'Confirm Password must be at most 12 characters long' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
+
+export const signInSchema = z.object({
+  email: z.email({ error: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { error: 'Password must be at least 6 characters long' })
+    .max(12, { error: 'Password must be at most 12 characters long' }),
+});
+
+export type SignUpSchemaType = z.infer<typeof signUpSchema>;
+export type SignInSchemaType = z.infer<typeof signInSchema>;
